59981f7637358ec1c57376d3f805738afcadb160
Ricardo R. M <riba.hdm@gmail.com>
2022-04-21 13:35:37 -0300

    Refatoração

--- app.js
+++ app.js
@@ -4,15 +4,30 @@ const searchInput = document.querySelector(".form-search input")
 
 
 const createNewToDo = toDoText => {
+  const datasetId = toDoText.replace(' ', '')
   return `
-  <li class="list-group-item d-flex justify-content-between align-items-center">
+  <li class="list-group-item d-flex justify-content-between align-items-center"
+  id=${datasetId}>
     <span>${toDoText}</span>
-    <i class="far fa-trash-alt delete"></i>
+    <i class="far fa-trash-alt delete" data-id=${datasetId}></i>
   </li>`
+}
+
+const toggleToDos = (searchText, toDoList) => {
+  toDoList.forEach(todo => {
+    if (todo.textContent.toLowerCase().includes(searchText)) {
+      todo.classList.add('d-flex')
+      todo.classList.remove('hidden')
+      return
+    }
+    todo.classList.remove('d-flex')
+    todo.classList.add('hidden')
+  })
 }
 
 addForm.addEventListener('submit', event => {
   event.preventDefault()
+
   const newToDo = event.target.add.value.trim()
 
   toDoListContainer.innerHTML += createNewToDo(newToDo)
@@ -24,7 +39,8 @@ toDoListContainer.addEventListener('click', event => {
   const classList = Array.from(event.target.classList)
 
   if (classList.includes('delete')) {
-    event.target.parentElement.remove()
+    const deleteId = event.target.dataset.id
+    document.querySelector(`#${deleteId}`).remove()
   }
 })
 
@@ -32,13 +48,5 @@ searchInput.addEventListener('input', event => {
   const searchText = event.target.value.toLowerCase()
   const toDoList = Array.from(toDoListContainer.children)
 
-  toDoList.forEach(todo => {
-    if (todo.textContent.toLowerCase().includes(searchText)) {
-      todo.classList.add('d-flex')
-      todo.classList.remove('hidden')
-      return
-    }
-    todo.classList.remove('d-flex')
-    todo.classList.add('hidden')
-  })
+  toggleToDos(searchText, toDoList)
 })
